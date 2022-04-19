b1d3f30672e525d2f0f49de47d2142d724760941
Kayzwer Lee <lkyu753@gmail.com>
2022-04-19 20:42:22 +0800

    categorical dqn

--- Categorical DQN.py
+++ Categorical DQN.py
@@ -1,174 +1,296 @@
+from typing import Dict, Tuple
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
-import pickle
-import time
 import gym
 
 
-class Network(nn.Module):
-    def __init__(self, input_dims, output_dims, atom_size, learning_rate, support) -> None:
-        super(Network, self).__init__()
-        self.support = support
-        self.output_dims = output_dims
-        self.atom_size = atom_size
+class ReplayBuffer:
+    """A simple numpy replay buffer."""
 
+    def __init__(self, obs_dim: int, size: int, batch_size: int = 32):
+        self.obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.next_obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.acts_buf = np.zeros([size], dtype=np.float32)
+        self.rews_buf = np.zeros([size], dtype=np.float32)
+        self.done_buf = np.zeros(size, dtype=np.float32)
+        self.max_size, self.batch_size = size, batch_size
+        self.ptr, self.size, = 0, 0
+
+    def store(
+        self, 
+        obs: np.ndarray, 
+        act: np.ndarray, 
+        rew: float, 
+        next_obs: np.ndarray, 
+        done: bool,
+    ):
+        self.obs_buf[self.ptr] = obs
+        self.next_obs_buf[self.ptr] = next_obs
+        self.acts_buf[self.ptr] = act
+        self.rews_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample_batch(self) -> Dict[str, np.ndarray]:
+        idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
+        return dict(obs=self.obs_buf[idxs],
+                    next_obs=self.next_obs_buf[idxs],
+                    acts=self.acts_buf[idxs],
+                    rews=self.rews_buf[idxs],
+                    done=self.done_buf[idxs])
+
+    def __len__(self) -> int:
+        return self.size
+
+
+class Network(nn.Module):
+    def __init__(
+        self, 
+        in_dim: int, 
+        out_dim: int, 
+        atom_size: int, 
+        support: torch.Tensor
+    ):
+        """Initialization."""
+        super(Network, self).__init__()
+
+        self.support = support
+        self.out_dim = out_dim
+        self.atom_size = atom_size
+        
         self.layers = nn.Sequential(
-            nn.Linear(input_dims, 64),
+            nn.Linear(in_dim, 128), 
             nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, output_dims * atom_size)
+            nn.Linear(128, 128), 
+            nn.ReLU(), 
+            nn.Linear(128, out_dim * atom_size)
         )
-        self.optimizer = optim.Adam(self.parameters(), learning_rate)
-    
-    def forward(self, x):
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
         dist = self.dist(x)
-        q = torch.sum(dist * self.support, dim = 2)
+        q = torch.sum(dist * self.support, dim=2)
+        
         return q
     
-    def dist(self, x):
-        q_atoms = self.layers.forward(x).view(-1, self.output_dims, self.atom_size)
-        dist = F.softmax(q_atoms, dim = -1)
-        dist = dist.clamp(min = 1e-3)
+    def dist(self, x: torch.Tensor) -> torch.Tensor:
+        """Get distribution for atoms."""
+        q_atoms = self.layers(x).view(-1, self.out_dim, self.atom_size)
+        dist = F.softmax(q_atoms, dim=-1)
+        dist = dist.clamp(min=1e-3)  # for avoiding nans
+        
         return dist
 
 
-class Replay_Buffer:
-    def __init__(self, input_dims, buffer_size, batch_size):
-        self.counter = 0
-        self.buffer_size = buffer_size
+class DQNAgent:
+    """DQN Agent interacting with environment.
+    
+    Attribute:
+        env (gym.Env): openAI Gym environment
+        memory (ReplayBuffer): replay memory to store transitions
+        batch_size (int): batch size for sampling
+        epsilon (float): parameter for epsilon greedy policy
+        epsilon_decay (float): step size to decrease epsilon
+        max_epsilon (float): max value of epsilon
+        min_epsilon (float): min value of epsilon
+        target_update (int): period for target model's hard update
+        gamma (float): discount factor
+        dqn (Network): model to train and select actions
+        dqn_target (Network): target model to update
+        optimizer (torch.optim): optimizer for training dqn
+        transition (list): transition information including
+                           state, action, reward, next_state, done
+        v_min (float): min value of support
+        v_max (float): max value of support
+        atom_size (int): the unit number of support
+        support (torch.Tensor): support for categorical dqn
+    """
+
+    def __init__(
+        self, 
+        env: gym.Env,
+        memory_size: int,
+        batch_size: int,
+        target_update: int,
+        epsilon_decay: float,
+        max_epsilon: float = 1.0,
+        min_epsilon: float = 0.1,
+        gamma: float = 0.99,
+        # Categorical DQN parameters
+        v_min: float = 0.0,
+        v_max: float = 500.0,
+        atom_size: int = 51,
+    ):
+        """Initialization.
+        
+        Args:
+            env (gym.Env): openAI Gym environment
+            memory_size (int): length of memory
+            batch_size (int): batch size for sampling
+            target_update (int): period for target model's hard update
+            epsilon_decay (float): step size to decrease epsilon
+            lr (float): learning rate
+            max_epsilon (float): max value of epsilon
+            min_epsilon (float): min value of epsilon
+            gamma (float): discount factor
+            v_min (float): min value of support
+            v_max (float): max value of support
+            atom_size (int): the unit number of support
+        """
+        obs_dim = env.observation_space.shape[0]
+        action_dim = env.action_space.n
+        
+        self.env = env
+        self.memory = ReplayBuffer(obs_dim, memory_size, batch_size)
         self.batch_size = batch_size
-        self.state_memory = np.zeros((buffer_size, *input_dims), dtype = np.float32)
-        self.action_memory = np.zeros(buffer_size, dtype = np.int8)
-        self.reward_memory = np.zeros(buffer_size, dtype = np.float32)
-        self.next_state_memory = np.zeros((buffer_size, *input_dims), dtype = np.float32)
-        self.terminal_state_memory = np.zeros(buffer_size, dtype = np.bool8)
-    
-    def store(self, state, action, reward, next_state, is_done):
-        if self.is_full():
-            return None
-        self.state_memory[self.counter] = state
-        self.action_memory[self.counter] = action
-        self.reward_memory[self.counter] = reward
-        self.next_state_memory[self.counter] = next_state
-        self.terminal_state_memory[self.counter] = is_done
-        self.counter += 1
-    
-    def sample_batch(self):
-        batch_index = np.random.choice(self.buffer_size, self.batch_size, False)
-        return dict(
-            state_batch = torch.tensor(self.state_memory[batch_index], dtype = torch.float32),
-            action_batch = torch.tensor(self.action_memory[batch_index], dtype = torch.int64),
-            reward_batch = torch.tensor(self.reward_memory[batch_index], dtype = torch.float32),
-            next_state_batch = torch.tensor(self.next_state_memory[batch_index], dtype = torch.float32),
-            terminal_state_batch = torch.tensor(self.terminal_state_memory[batch_index], dtype = torch.bool)
-        )
-    
-    def reset_buffer(self):
-        self.counter = 0
-        self.state_memory.fill(0)
-        self.action_memory.fill(0)
-        self.reward_memory.fill(0)
-        self.next_state_memory.fill(0)
-        self.terminal_state_memory.fill(0)
-    
-    def is_full(self):
-        return self.counter >= self.buffer_size
-
-
-class Epsilon_Controller:
-    def __init__(self, epsilon, epsilon_decay_rate, minimum_epsilon, reward_target, reward_target_grow_rate, confidence):
-        self.epsilon = epsilon
-        self.epsilon_decay_rate = epsilon_decay_rate
-        self.minimum_epsilon = minimum_epsilon
-        self.reward_target = reward_target
-        self.reward_target_grow_rate = reward_target_grow_rate
-        self.confidence = confidence
-        self.confidence_count = 0
-        self.deci_place = self._get_deci_place()
-    
-    def decay(self, last_reward):
-        if self.epsilon > self.minimum_epsilon and last_reward >= self.reward_target:
-            if self.confidence_count < self.confidence:
-                self.confidence_count += 1
-            else:
-                self.epsilon = round(self.epsilon - self.epsilon_decay_rate, self.deci_place) if self.epsilon > self.minimum_epsilon else self.minimum_epsilon
-                self.reward_target += self.reward_target_grow_rate
-                self.confidence_count = 0
-        else:
-            self.confidence_count = 0
-    
-    def _get_deci_place(self) -> int:
-        count = 0
-        after_dot = False
-        for i in self.epsilon_decay_rate:
-            if after_dot:
-                count += 1
-            if i == ".":
-                after_dot = True
-        self.epsilon_decay_rate = float(self.epsilon_decay_rate)
-        return count
-
-
-class Agent:
-    def __init__(self, input_dims, output_dims, atom_size, v_min, v_max, learning_rate, gamma, tau, buffer_size, batch_size, epsilon, epsilon_decay_rate, minimum_epsilon, reward_target, reward_target_grow_rate, confidence):
-        self.input_dims = input_dims
-        self.output_dims = output_dims
-        self.learning_rate = learning_rate
+        self.epsilon = max_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.max_epsilon = max_epsilon
+        self.min_epsilon = min_epsilon
+        self.target_update = target_update
         self.gamma = gamma
-        self.tau = tau
+        
+        # Categorical DQN parameters
         self.v_min = v_min
         self.v_max = v_max
-        self.network = Network(input_dims, output_dims, learning_rate, atom_size)
-        self.target_network = Network(input_dims, output_dims, learning_rate, atom_size)
-        self.buffer = Replay_Buffer(input_dims, buffer_size, batch_size)
-        self.epsilon_controller = Epsilon_Controller(epsilon, epsilon_decay_rate, minimum_epsilon, reward_target, reward_target_grow_rate, confidence)
-        self.support = torch.linspace(v_min, v_max, atom_size)
-        self.target_network.load_state_dict(self.network.state_dict())
+        self.atom_size = atom_size
+        self.support = torch.linspace(
+            self.v_min, self.v_max, self.atom_size
+        )
 
-    def choose_action(self, state):
-        if np.random.random() > self.epsilon_controller.epsilon:
-            return torch.argmax(self.network.forward(torch.tensor(state))).item()
+        # networks: dqn, dqn_target
+        self.dqn = Network(
+            obs_dim, action_dim, atom_size, self.support
+        )
+        self.dqn_target = Network(
+            obs_dim, action_dim, atom_size, self.support
+        )
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+        self.dqn_target.eval()
+        
+        # optimizer
+        self.optimizer = optim.Adam(self.dqn.parameters())
+
+        # transition to store in memory
+        self.transition = list()
+        
+        # mode: train / test
+        self.is_test = False
+
+    def select_action(self, state: np.ndarray) -> np.ndarray:
+        """Select an action from the input state."""
+        # epsilon greedy policy
+        if self.epsilon > np.random.random():
+            selected_action = self.env.action_space.sample()
         else:
-            return np.random.choice(self.output_dims)
-    
-    def SHIN_choose_action(self, state):
-        return torch.argmax(self.network.forward(torch.tensor(state))).item()
-    
-    def update_network(self):
-        for target_network_param, network_param in zip(self.target_network.parameters(), self.network.parameters()):
-            target_network_param.data.copy_(self.tau * network_param + (1 - self.tau) * target_network_param)
+            selected_action = self.dqn(
+                torch.FloatTensor(state),
+            ).argmax()
+            selected_action = selected_action.detach().cpu().numpy()
+        
+        if not self.is_test:
+            self.transition = [state, selected_action]
+        
+        return selected_action
 
-    def learn(self, env):
-        batch = self.buffer.sample_batch()
-        state_batch = batch.get("state_batch")
-        action_batch = batch.get("action_batch")
-        reward_batch = batch.get("reward_batch")
-        next_state_batch = batch.get("next_state_batch")
-        terminal_state_batch = batch.get("terminal_state_batch")
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, np.float64, bool]:
+        """Take an action and return the response of the env."""
+        next_state, reward, done, _ = self.env.step(action)
 
-        self.network.zero_grad()
-        delta_z = (self.v_max - self.v_min) / (self.network.atom_size - 1)
+        if not self.is_test:
+            self.transition += [reward, next_state, done]
+            self.memory.store(*self.transition)
+    
+        return next_state, reward, done
+
+    def update_model(self) -> torch.Tensor:
+        """Update the model by gradient descent."""
+        samples = self.memory.sample_batch()
+
+        loss = self._compute_dqn_loss(samples)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+        
+    def train(self, num_frames: int):
+        """Train the agent."""
+        self.is_test = False
+        
+        state = self.env.reset()
+        update_cnt = 0
+        epsilons = []
+        losses = []
+        scores = []
+        score = 0
+
+        for frame_idx in range(1, num_frames + 1):
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+            # if episode ends
+            if done:
+                state = self.env.reset()
+                scores.append(score)
+                score = 0
+
+            # if training is ready
+            if len(self.memory) >= self.batch_size:
+                loss = self.update_model()
+                losses.append(loss)
+                update_cnt += 1
+                
+                # linearly decrease epsilon
+                self.epsilon = max(
+                    self.min_epsilon, self.epsilon - (
+                        self.max_epsilon - self.min_epsilon
+                    ) * self.epsilon_decay
+                )
+                epsilons.append(self.epsilon)
+                
+                # if hard update is needed
+                if update_cnt % self.target_update == 0:
+                    self._target_hard_update()
+            print(f"Iteration: {frame_idx}")
+        self.env.close()
+
+    def _compute_dqn_loss(self, samples: Dict[str, np.ndarray]) -> torch.Tensor:
+        """Return categorical dqn loss."""
+        state = torch.FloatTensor(samples["obs"])
+        next_state = torch.FloatTensor(samples["next_obs"])
+        action = torch.LongTensor(samples["acts"])
+        reward = torch.FloatTensor(samples["rews"].reshape(-1, 1))
+        done = torch.FloatTensor(samples["done"].reshape(-1, 1))
+        
+        # Categorical DQN algorithm
+        delta_z = float(self.v_max - self.v_min) / (self.atom_size - 1)
+
         with torch.no_grad():
-            next_action = self.target_network.forward(next_state_batch).argmax(1)
-            next_dist = self.target_network.dist(next_state_batch)
-            next_dist = next_dist[range(self.buffer.batch_size), next_action]
+            next_action = self.dqn_target(next_state).argmax(1)
+            next_dist = self.dqn_target.dist(next_state)
+            next_dist = next_dist[range(self.batch_size), next_action]
 
-            t_z = reward_batch + ~terminal_state_batch * self.gamma * self.support
-            t_z.clamp(min = self.v_min, max = self.v_max)
+            t_z = reward + (1 - done) * self.gamma * self.support
+            t_z = t_z.clamp(min=self.v_min, max=self.v_max)
             b = (t_z - self.v_min) / delta_z
             l = b.floor().long()
             u = b.ceil().long()
 
             offset = (
                 torch.linspace(
-                    0, (self.buffer.batch_size - 1) * self.network.atom_size, self.buffer.batch_size
+                    0, (self.batch_size - 1) * self.atom_size, self.batch_size
                 ).long()
                 .unsqueeze(1)
-                .expand(self.buffer.batch_size, self.network.atom_size)
+                .expand(self.batch_size, self.atom_size)
             )
 
             proj_dist = torch.zeros(next_dist.size())
@@ -178,32 +300,35 @@ class Agent:
             proj_dist.view(-1).index_add_(
                 0, (u + offset).view(-1), (next_dist * (b - l.float())).view(-1)
             )
-        dist = self.network.dist(state_batch)
-        log_p = torch.log(dist[range(self.buffer.batch_size), action_batch])
-        loss = -(proj_dist * log_p).sum(dim = 1).mean()
-        loss.backward()
-        self.network.optimizer.step()
-    
-        score = 0
-        is_done = False
-        state = env.reset()
-        while not is_done:
-            action = self.SHIN_choose_action(state)
-            state, reward, is_done, _ = env.step(action)
-            score += reward
-        self.epsilon_controller.decay(score)
-        return score
 
-    def test(self, env, n_games):
-        for i in range(n_games):
-            score = 0
-            is_done = False
-            state = env.reset()
-            while not is_done:
-                time.sleep(0.01)
-                env.render()
-                action = self.SHIN_choose_action(state)
-                state, reward, is_done, _ = env.step(action)
-                score += reward
-            print(f"Game: {i + 1}, Score: {score}")
-        env.close()
+        dist = self.dqn.dist(state)
+        log_p = torch.log(dist[range(self.batch_size), action])
+
+        loss = -(proj_dist * log_p).sum(1).mean()
+
+        return loss
+
+    def _target_hard_update(self):
+        """Hard update: target <- local."""
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v1")
+    # parameters
+    num_frames = 20000
+    memory_size = 2000
+    batch_size = 32
+    target_update = 200
+    epsilon_decay = 1 / 2000
+
+    # train
+    agent = DQNAgent(env, memory_size, batch_size, target_update, epsilon_decay)
+    agent.train(num_frames)
+
+    state = env.reset()
+    done = False
+    while not done:
+        env.render()
+        action = agent.select_action(state)
+        state, reward, done, _ = env.step(action)
